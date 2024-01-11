1b1faac2a1d4d1ffd3ed87740a3fa0ba091db391
mifz07 <taofik.miftah@gmail.com>
2024-01-11 21:44:15 +0700

    add ruting filter

--- src/router/index.js
+++ src/router/index.js
@@ -34,4 +34,19 @@ const router = createRouter({
   routes,
 });
 
+//handle unauthorized user
+router.beforeEach((to, from, next) => {
+    const publicPages = ['/login', '/register', '/home'];
+    const authRequired = !publicPages.includes(to.path);
+    const loggedIn = localStorage.getItem('user');
+  
+    // trying to access a restricted page + not logged in
+    // redirect to login page
+    if (authRequired && !loggedIn) {
+      next('/login');
+    } else {
+      next();
+    }
+});
+
 export default router;
